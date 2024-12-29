13726d95db2b377752bb2f9d514969d616ceb665
Nick <nickaquilla4@gmail.com>
2024-12-28 19:13:43 -0600

    Fix add task bug/task would not add to list

--- todo.py
+++ todo.py
@@ -1,17 +1,24 @@
 def display_menu():
-    print("1. View tasks")
+    print("\n1.  View tasks")
     print("2. Create a new task")
     print("3. Delete a new task")
     print("4. Exit")
 
+def view_task(tasks):
+    if not tasks:
+        print("\n No Tasks!")
+    else:
+        print("Your Tasks: ")
+        for i, (tasks, completed) in enumerate (tasks, 1):
+            status = "c" if completed else "N"
+            print(f"{i}. {tasks} [{status}]")
+    
 def add_task(tasks):
     task = input("\n Enter your task")
-    tasks.append(task, False)
+    tasks.append((task, False))
     print("Tasked added sucessfully")
 
-def view_task():
-    current = add_task()
-    print(current)
+
 
 def delete_task(tasks):
    task = tasks.remove()
@@ -21,16 +28,19 @@ def main() :
     tasks = []
     while True:
         display_menu()
-        choice = input("\n Enter your choice")
-
-        if choice == 1 :
-            view_task()
-        elif choice == 2 :
-            add_task()
-        elif choice == 3 :
-            delete_task()
-        elif choice == 4:
+        choice = input("\n Enter your option: ")
+        if choice == "1":
+            view_task(tasks)
+        elif choice == "2":
+            add_task(tasks)
+        elif choice == "3" :
+            delete_task(tasks)
+        elif choice == "4":
             print("Exit the application!")
+            break
+        else:
+            print("Invalid entry, try again")
+
             
 
 # Why assign the list function in a if statement?
